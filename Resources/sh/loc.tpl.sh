06dc4764f376733d8e26221f69b037883ac3f5eb
jdlabails <jd.labails@obiz.fr>
2017-11-04 15:09:30 +0100

    phploc as dependency

--- Resources/sh/loc.tpl.sh
+++ Resources/sh/loc.tpl.sh
@@ -2,7 +2,7 @@
 rm -f ${DIR_REPORT}/LOC/*.txt
 
 echo "Mesures des sources par PhpLoc"
-echo "php ${DIR_PHAR}/phploc.phar --log-xml ${DIR_REPORT}/LOC/phploc.xml ${DIR_SRC}" > ${DIR_REPORT}/LOC/cmd.txt
-echo "php ${DIR_PHAR}/phploc.phar ${DIR_SRC}" > ${DIR_REPORT}/LOC/cmdManuelle.txt
+echo "php ${DIR_BIN}/phploc --log-xml ${DIR_REPORT}/LOC/phploc.xml ${DIR_SRC}" > ${DIR_REPORT}/LOC/cmd.txt
+echo "php ${DIR_BIN}/phploc ${DIR_SRC}" > ${DIR_REPORT}/LOC/cmdManuelle.txt
 
-php ${DIR_PHAR}/phploc.phar --log-xml ${DIR_REPORT}/LOC/phploc.xml ${DIR_SRC} > ${DIR_REPORT}/LOC/report.txt 2>&1
+php ${DIR_BIN}/phploc --log-xml ${DIR_REPORT}/LOC/phploc.xml ${DIR_SRC} > ${DIR_REPORT}/LOC/report.txt 2>&1

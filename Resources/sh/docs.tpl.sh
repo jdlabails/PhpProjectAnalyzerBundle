7842ada2950d6575d6fc84fe5681cda2e6f57c32
jdlabails <jd.labails@obiz.fr>
2017-11-04 22:23:59 +0100

    phpmd as dependencies

--- Resources/sh/docs.tpl.sh
+++ Resources/sh/docs.tpl.sh
@@ -3,6 +3,6 @@ if [ ${DOC} -eq 1 ]
 then
     rm -rf ${DIR_REPORT}/DOCS/*
     echo "Rédaction de la documentation"
-    echo "php ${DIR_PHAR}/phpDocumentor.phar -d ${DIR_SRC} -t ${DIR_REPORT}/DOCS" > ${DIR_REPORT}/DOCS/cmd.txt
-    php ${DIR_PHAR}/phpDocumentor.phar -d ${DIR_SRC} -t ${DIR_REPORT}/DOCS > ${DIR_REPORT}/DOCS/report.txt 2>&1
+    echo "${DIR_BIN}/phpDocumentor -d ${DIR_SRC} -t ${DIR_REPORT}/DOCS" > ${DIR_REPORT}/DOCS/cmd.txt
+    ${DIR_BIN}/phpDocumentor -d ${DIR_SRC} -t ${DIR_REPORT}/DOCS > ${DIR_REPORT}/DOCS/report.txt 2>&1
 fi
